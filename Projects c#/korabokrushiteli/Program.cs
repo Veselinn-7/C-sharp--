32ead2bd033805723a66760ef4d510c64773ce84
Veselinn-7 <vesselinnk@gmail.com>
2023-10-12 15:40:36 +0300

    First project
    Create a List of strings and add numbers to that list while using a for loop. Use a string type in order to type "done" after adding the numbers. After "done" is typed use an if statement and sort function to sort the string of numbers in the list from smallest to largest. Make sure to remove the "done" string in the list so it doesn't appear after using a foreach loop to print the sorted numbers after that.

---
+++ Projects c#/korabokrushiteli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace korabokrushiteli
+
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            
+            List<string> ages = new List<string>();
+            for(int i = 0; i <= ages.Count; i++)
+            {
+                string number = Console.ReadLine();
+                ages.Add(number);
+                if(number == "done")
+                {
+                    ages.Sort();
+                    ages.Remove("done");
+                    foreach(string text in ages)
+                    {
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+            
+
+
+
+
+        }                        
+    }
+}
+
+
