d95d55b331c893f65eb8bbe27411e6e5b2397267
Veselinn-7 <vesselinnk@gmail.com>
2023-10-13 17:17:51 +0300

    Using and practicing on "Lists" - finding the average of all elements
    Initiating with an integer of sum to store the values. Opening a while statement to run. Inputting a string and adding it to an if statement. Using a parse method to check if the string can be parsed into an int. If yes, to add that value to the list as an int. After it is added sum takes the value of every number typed and prints the sum of the numbers. When "done" is typed the last result if the total sum of all the numbers divided by their count.

---
+++ Projects c#/SolutionsToProblemss/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionsToProblemss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+                List<int> backpacks = new List<int>();
+                int sum = 0;
+                bool isRunning = true;
+                while (isRunning == true)
+               {
+           
+                    string input = Console.ReadLine();
+                
+                if (int.TryParse(input, out int count))
+                {
+                    backpacks.Add(count);
+                    sum = sum + count;
+                }
+                else if (input == "done")
+                {
+                    int result = 0;
+                    result = sum / backpacks.Count;
+                    Console.WriteLine(result);
+                    break;
+                        
+                }    
+
+            }
+
+          }      
+
+        }
+    }
+
